ded9deea65fef5454aad170138ec665e8c51b337
dongilc <dongilc@mju.ac.kr>
2021-04-29 13:58:46 +0900

    20210429

--- intelligent_robotics.py
+++ intelligent_robotics.py
@@ -13,6 +13,7 @@ from sympy.physics.vector import dynamicsymbols
 from sympy.physics.vector import time_derivative
 from sympy.physics.vector import ReferenceFrame
 N = ReferenceFrame('N')
+import math, numpy
 
 ###### 동차변환
 # DH 파라미터를 이용해 Homogeneous Transformation Matrix 만드는 함수
@@ -297,3 +298,116 @@ def get_Christoffel_term(M,q,qd):
         C[i] = sympy.simplify(qd.T@Gamma_i@qd);
         
     return Gamma, C
+
+##### 곡선계획법 Trajectory Planning
+##### Reference : Modern Robotics Textbook, Chapter 9. Trapezoidal Motion Profile (335 page)
+# 사다리꼴 속도 계획법 - 속도최대값과 가속도최대값을 입력으로
+def Trapezoidal_Traj_Gen_Given_Vmax_and_Amax(vmax,amax,dt):
+    v = vmax
+    a = amax
+    
+    if math.pow(v,2)/a > 1:
+        return False
+    
+    T = (a + math.pow(v,2))/(v*a)
+    
+    time = 0
+    t_save = time
+    traj_save = numpy.array([0,0,0])
+    while T >= time:
+        if time >= 0 and time <= (v/a):
+            sddot = a
+            sdot = a*time
+            s = 0.5*a*math.pow(time,2)
+            
+        if time > (v/a) and time <= (T-v/a):
+            sddot = 0
+            sdot = v 
+            s = v*time - 0.5*math.pow(v,2)/a
+            
+        if time > (T-v/a) and time <= T:
+            sddot = -a
+            sdot = a*(T-time)
+            s = (2*a*v*T - 2*math.pow(v,2) - math.pow(a,2)*math.pow((time-T),2))/(2*a)
+        
+        t_save = numpy.vstack((t_save, time))
+        traj_save = numpy.vstack((traj_save, numpy.array([s,sdot,sddot])))
+        time += dt
+
+    return t_save, traj_save
+
+##### 곡선계획법 Trajectory Planning
+# 사다리꼴 속도 계획법 - 속도최대값과 최종시간을 입력으로
+def Trapezoidal_Traj_Gen_Given_Vmax_and_T(vmax,T,dt):
+    v = vmax
+    
+    if v*T > 1 and v*T <= 2:     
+        a = math.pow(v,2)/(v*T-1)
+    else:
+        return False, False            
+    
+    time = 0
+    t_save = time
+    traj_save = numpy.array([0,0,0])
+    while T >= time:
+        if time >= 0 and time <= (v/a):
+            sddot = a
+            sdot = a*time
+            s = 0.5*a*math.pow(time,2)
+            
+        if time > (v/a) and time <= (T-v/a):
+            sddot = 0
+            sdot = v 
+            s = v*time - 0.5*math.pow(v,2)/a
+            
+        if time > (T-v/a) and time <= T:
+            sddot = -a
+            sdot = a*(T-time)
+            s = (2*a*v*T - 2*math.pow(v,2) - math.pow(a,2)*math.pow((time-T),2))/(2*a)
+        
+        t_save = numpy.vstack((t_save, time))
+        traj_save = numpy.vstack((traj_save, numpy.array([s,sdot,sddot])))
+        time += dt
+
+    return t_save, traj_save
+
+##### 곡선계획법 Trajectory Planning
+# 사다리꼴 속도 계획법 - 가속도최대값과 최종시간을 입력으로
+def Trapezoidal_Traj_Gen_Given_Amax_and_T(amax,T,dt):
+    a = amax
+    
+    if a*math.pow(T,2) >= 4:     
+        v = 0.5*(a*T - math.pow(a,0.5)*math.pow((a*math.pow(T,2)-4),0.5))
+    else:
+        return False, False            
+    
+    time = 0
+    t_save = time
+    traj_save = numpy.array([0,0,0])
+    while T >= time:
+        if time >= 0 and time <= (v/a):
+            sddot = a
+            sdot = a*time
+            s = 0.5*a*math.pow(time,2)
+            
+        if time > (v/a) and time <= (T-v/a):
+            sddot = 0
+            sdot = v 
+            s = v*time - 0.5*math.pow(v,2)/a
+            
+        if time > (T-v/a) and time <= T:
+            sddot = -a
+            sdot = a*(T-time)
+            s = (2*a*v*T - 2*math.pow(v,2) - math.pow(a,2)*math.pow((time-T),2))/(2*a)
+        
+        t_save = numpy.vstack((t_save, time))
+        traj_save = numpy.vstack((traj_save, numpy.array([s,sdot,sddot])))
+        time += dt
+
+    return t_save, traj_save
+
+##### 곡선계획법 Trajectory Planning
+# 0~1범위의 trajectory를 기반으로 실질적인 Path를 생성
+def Path_Gen(start,goal,traj):
+    path = start + traj*(goal-start)
+    return path   
